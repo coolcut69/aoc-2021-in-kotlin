f0639424744dccbed832ecdf9045e04906845367
cborghmans <chris.borghmans@dpgmedia.be>
2021-12-02 07:29:46 +0100

    feat: day two, part two

--- src/Day02.kt
+++ src/Day02.kt
@@ -12,13 +12,21 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val instructions = input.map { SubInstruction(it.split(" ").toTypedArray()) }
+
+        val submarine = Submarine()
+
+        for (instruction in instructions) {
+            submarine.move(instruction)
+        }
+
+        return submarine.horizontalPosition * submarine.depth
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 150)
-    check(part2(testInput) == 6)
+//    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println(part1(input))
@@ -26,7 +34,6 @@ fun main() {
 }
 
 class SubInstruction(toTypedArray: Array<String>) {
-
     var type = ""
     var size = 0
 
@@ -34,25 +41,26 @@ class SubInstruction(toTypedArray: Array<String>) {
         type = toTypedArray[0]
         size = toTypedArray[1].toInt()
     }
-
 }
 
 class Submarine {
     var horizontalPosition = 0
     var depth = 0
+    var aim = 0
 
     fun move(instruction: SubInstruction) {
-        if (instruction.type == "forward") {
-            horizontalPosition += instruction.size
-        }
         if (instruction.type == "down") {
-            depth += instruction.size
+            aim += instruction.size
         }
         if (instruction.type == "up") {
-            depth -= instruction.size
+            aim -= instruction.size
+        }
+        if (instruction.type == "forward") {
+            horizontalPosition += instruction.size
+            if (aim != 0) {
+                depth += aim * instruction.size
+            }
         }
     }
-
-
 }
 
