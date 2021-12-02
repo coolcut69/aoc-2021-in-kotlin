396a09d48d31899248d974fa5bc9089c82354ef9
cborghmans <chris.borghmans@dpgmedia.be>
2021-12-02 21:05:34 +0100

    refactor: use kotlin standard lib features

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,31 +1,12 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val numbers = input.map(String::toInt)
-        var count = 0
-
-        for ((index, value) in numbers.withIndex()) {
-            if (index+1  < numbers.size && numbers[index + 1] > value) {
-                count++
-            }
-        }
-        return count
+        return numbers.windowed(2).count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
         val numbers = input.map(String::toInt)
-        val measurements = mutableListOf<ThreePointMeasurement>()
-
-        for ((index, _) in numbers.withIndex()) {
-            measurements.add(ThreePointMeasurement(index, numbers))
-        }
-
-        var count = 0
-        for ((index, value) in measurements.withIndex()) {
-            if (index + 1 < numbers.size && measurements[index + 1].numbers.sum() > value.numbers.sum()) {
-                count++
-            }
-        }
-        return count
+        return numbers.windowed(3).windowed(2).count { (a, b) -> b.sum() > a.sum() }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -36,20 +17,5 @@ fun main() {
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
-}
-
-class ThreePointMeasurement(index: Int, inputList: List<Int>) {
-    val numbers = mutableListOf<Int>()
-
-    init {
-        numbers.add(inputList[index])
-        if (index + 1 < inputList.size) {
-            numbers.add(inputList[index + 1])
-
-        }
-        if (index + 2 < inputList.size) {
-            numbers.add(inputList[index + 2])
-        }
-    }
 }
 
