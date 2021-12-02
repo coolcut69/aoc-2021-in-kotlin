1b14f22e6ed1cb2ad1922d23961d2d1d2c5d6b99
cborghmans <chris.borghmans@dpgmedia.be>
2021-12-02 07:22:14 +0100

    feat: day two, part one

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val instructions = input.map { SubInstruction(it.split(" ").toTypedArray()) }
+
+        val submarine = Submarine()
+
+        for (instruction in instructions) {
+            submarine.move(instruction)
+        }
+
+        return submarine.horizontalPosition * submarine.depth
     }
 
     fun part2(input: List<String>): Int {
@@ -9,11 +17,42 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 6)
 
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
+}
+
+class SubInstruction(toTypedArray: Array<String>) {
+
+    var type = ""
+    var size = 0
+
+    init {
+        type = toTypedArray[0]
+        size = toTypedArray[1].toInt()
+    }
+
+}
+
+class Submarine {
+    var horizontalPosition = 0
+    var depth = 0
+
+    fun move(instruction: SubInstruction) {
+        if (instruction.type == "forward") {
+            horizontalPosition += instruction.size
+        }
+        if (instruction.type == "down") {
+            depth += instruction.size
+        }
+        if (instruction.type == "up") {
+            depth -= instruction.size
+        }
+    }
+
+
 }
 
