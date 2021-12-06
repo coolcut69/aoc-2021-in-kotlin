2e65d02ebadfe560fedf79ea70202b703d33907c
cborghmans <chris.borghmans@dpgmedia.be>
2021-12-06 09:08:13 +0100

    feat: day six, part one

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>, days: Int): Int {
+        val map = input[0].split(",").map { it.toInt() }.map { Lanternfish(it, true) }
+        val fishes: MutableList<Lanternfish> = ArrayList(map)
+
+        repeat(days) {
+            val newFishes = fishes.count { it.age == 0 }
+            repeat(newFishes) {
+                fishes.add(Lanternfish(8, false))
+            }
+            fishes.map { fish -> fish.age() }
+        }
+        return fishes.count()
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>, days: Int): Long {
         return 0
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput, 80) == 5934)
+//    check(part2(testInput, 256) == 26984457539)
 
     val input = readInput("Day06")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input, 80))
+//    println(part2(input, 256))
 }
 
+class Lanternfish(var age: Int, var canAge: Boolean) {
+
+    fun age() {
+        when {
+            age == 0 -> age = 6
+            canAge -> age--
+            else -> canAge = true
+        }
+    }
+}
