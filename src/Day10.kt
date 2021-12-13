e1ce6fb8401110fbcc64acaecaa75ef8cb15a42a
cborghmans <chris.borghmans@dpgmedia.be>
2021-12-13 09:07:54 +0100

    feat: day ten, part one

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,24 +1,72 @@
 fun main() {
     fun part1(input: List<String>): Int {
+
+        val corrupted = mutableListOf<String>()
         for (line in input) {
-            if (line.count { c -> c == '<' } != line.count { c -> c == '>' }) {
-                println("Expected < >")
-                break
-            }
-            if (line.count { c -> c == '[' } != line.count { c -> c == ']' }) {
-                println("Expected [ ]")
-                break
-            }
-            if (line.count { c -> c == '{' } != line.count { c -> c == '}' }) {
-                println("Expected { }")
-                break
-            }
-            if (line.count { c -> c == '(' } != line.count { c -> c == ')' }) {
-                println("Expected ( )")
-                break
+            var change: String = line
+
+            while (change.contains('}') || change.contains(']') || change.contains('>') || change.contains(')')) {
+                change = change.replace("[]", "")
+                change = change.replace("{}", "")
+                change = change.replace("<>", "")
+                change = change.replace("()", "")
+
+//                val numberOfEndChars = change.count { it == '}' } +
+//                        change.count { it == ']' } +
+//                        change.count { it == '>' } +
+//                        change.count { it == ')' }
+//                if (numberOfEndChars == 1) {
+                    var indexOf = change.indexOf(">")
+                    if (indexOf != -1){
+                        val possibleOpeningTag = change[indexOf -1]
+                        if (possibleOpeningTag == '{' || possibleOpeningTag == '('  || possibleOpeningTag == '[' ){
+                            corrupted.add("$possibleOpeningTag>")
+                            change = change.replace("$possibleOpeningTag>", "")
+                        }
+                    }
+                    indexOf = change.indexOf("}")
+                    if (indexOf != -1){
+                        val possibleOpeningTag = change[indexOf -1]
+                        if (possibleOpeningTag == '<' || possibleOpeningTag == '('  || possibleOpeningTag == '[' ){
+                            corrupted.add("$possibleOpeningTag}")
+                            change = change.replace("$possibleOpeningTag}", "")
+                        }
+                    }
+                    indexOf = change.indexOf("]")
+                    if (indexOf != -1){
+                        val possibleOpeningTag = change[indexOf -1]
+                        if (possibleOpeningTag == '{' || possibleOpeningTag == '('  || possibleOpeningTag == '<' ){
+                            corrupted.add("$possibleOpeningTag]")
+                            change = change.replace("$possibleOpeningTag]", "")
+                        }
+                    }
+                    indexOf = change.indexOf(")")
+                    if (indexOf != -1){
+                        val possibleOpeningTag = change[indexOf -1]
+                        if (possibleOpeningTag == '{' || possibleOpeningTag == '<'  || possibleOpeningTag == '[' ){
+                            corrupted.add("$possibleOpeningTag)")
+                            change = change.replace("$possibleOpeningTag)", "")
+                        }
+                    }
             }
         }
-        return 0
+        var errors = 0
+        for (line in corrupted){
+            if (line[1] == ')'){
+                errors += 3
+            }
+            if (line[1] == ']'){
+                errors += 57
+            }
+            if (line[1] == '}'){
+                errors += 1197
+            }
+            if (line[1] == '>'){
+                errors += 25137
+            }
+        }
+
+        return errors
     }
 
     fun part2(input: List<String>): Int {
@@ -27,11 +75,31 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 26397)
     check(part2(testInput) == 0)
 
     val input = readInput("Day10")
-//    println(part1(input))
+    println(part1(input))
 //    println(part2(input))
 }
 
+
+/*
+
+            if (line.count { c -> c == '<' } != line.count { c -> c == '>' }) {
+        println("Expected < >")
+//                break
+    } else if (line.count { c -> c == '[' } != line.count { c -> c == ']' }) {
+        println("Expected [ ]")
+//                break
+    } else if (line.count { c -> c == '{' } != line.count { c -> c == '}' }) {
+        println("Expected { }")
+//                break
+    } else if (line.count { c -> c == '(' } != line.count { c -> c == ')' }) {
+        println("Expected ( )")
+//                break
+    } else {
+        println("fine")
+
+
+ */
